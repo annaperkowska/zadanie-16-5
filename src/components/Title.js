f7d4ccd650eb48a1a09ed91bc115a972c485ed1d
Anna Perkowska <perkowska.a@gmail.com>
2018-04-11 00:34:58 +0200

    Create repo, copy files

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = props => 
+<div>
+	<h1>{props.title}</h1>
+	<h2>number of tasks</h2>
+</div>
+
+export default Title;
---
+++ src/containers/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+	addTodo(val){
+		const todo = {
+			text: val,
+			id: uuid.v4(),
+		};	
+		const data = [...this.state.data, todo];
+		this.setState({data});
+	}
+
+	removeTodo(id) {
+		const reminder = this.state.data.filter(todo => todo.id !== id);
+		this.setState({data:reminder});
+	}
+
+	render() {
+		return (
+			<div className={style.TodoApp}>
+				<Title title='To do list' />
+			</div>
+		);
+	}
+}
+
+export default App;
